1937b2cb0ea9cb8636e004bf6031fcedb4054222
Alaa-356 <alaaaemm@gmail.com>
2026-07-15 19:40:55 +0300

    alphabit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - is the entry point
+ * Description: print alphabet in lowercase, followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
