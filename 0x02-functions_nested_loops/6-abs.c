838312a4ddcc2d205f227227fcb0ea1eaf4b3fc2
Alaa-356 <alaaaemm@gmail.com>
2026-07-16 15:58:38 +0300

    print the absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - to print alphabet ac
+ * Description:  function that checks for lowercase character.
+ * @n: is variable take from the other function
+ * Return: o
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
