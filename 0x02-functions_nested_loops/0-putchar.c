9708b878ceb53e1eebb8d758102de22a32aef394
Alaa-356 <alaaaemm@gmail.com>
2026-07-16 12:14:48 +0300

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
  * Decription: to print _putchar folowed by new line
@@ -6,6 +6,11 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	int i;
+	char str[] = "_putchar";
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 	return (0);
 }
