ed716fd922707208831595a3bd5135a01344f5fe
Alaa-356 <alaaaemm@gmail.com>
2026-07-16 19:33:36 +0300

    add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - entry point
+ * Description: function that adds two integers and returns the result.
+ * @n: is variable
+ * @m: is variable
+ * Return: return result
+ */
+int add(int n, int m)
+{
+	int res;
+
+	res = n + m;
+	return (res);
+}
