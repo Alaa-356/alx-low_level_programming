d1d00c981aba5293873dcd6ab17ce059a410176d
Alaa-356 <alaaaemm@gmail.com>
2026-07-16 12:27:52 +0300

    print alphabit by _putchar 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - to print alphabet acc
+ * Description: to print alphabet acc 10 times
+ * Return: is 0
+ */
+void print_alphabet_x10(void)
+{
+	char let;
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		for (let = 'a'; let <= 'z'; let++)
+			_putchar(let);
+		_putchar('\n');
+	}
+}
