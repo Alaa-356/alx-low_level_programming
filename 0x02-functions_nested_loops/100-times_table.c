182406b0f7275da8bf330918937a906dfad60341
Alaa-356 <alaaaemm@gmail.com>
2026-07-17 15:02:54 +0300

    100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_times_table - is the program name
+ * Decription: If n is greater than 15 or less than 0
+ *             the function should not print anything
+ * @n: the vriable to be checked
+ * Return: is void
+ */
+void print_times_table(int n)
+{
+	int i, j, x;
+
+	if (n > 15 || n < 0)
+		return;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (i == 0 || j == 0)
+			{
+				if (j == n)
+					printf("%3d", 0);
+				else
+					printf("%3d, ", 0);
+			}
+			else
+			{
+				x = i * j;
+				if (j == n)
+					printf("%3d", x);
+				else
+					printf("%3d, ", x);
+			}
+			}
+		printf("\n");
+		}
+}
