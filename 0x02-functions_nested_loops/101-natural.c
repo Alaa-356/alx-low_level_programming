38786c8e96f2270dfd406c97782b75c14a23054b
Alaa-356 <alaaaemm@gmail.com>
2026-07-18 12:19:42 +0300

    program 101 advance

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: program to sum /3 and /5 and sum
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 1024, i, sum = 0;
+
+	for (i = 1; i < n; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+	}
+	printf("the sum of natural number of %d is %d\n", n, sum);
+	return (0);
+}
