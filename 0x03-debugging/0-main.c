f0b48cfaa847b7cf179d7f42465b6031ad540f8d
Alaa-356 <alaaaemm@gmail.com>
2026-07-18 18:33:40 +0300

    0-main in 0x03

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - is the entry point
+ * Description: to test +/- program for 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
