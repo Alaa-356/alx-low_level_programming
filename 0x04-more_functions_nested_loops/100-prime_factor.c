be7a8ffdc21c49a147c1474858fe58f8b0bbf116
Alaa-356 <alaaaemm@gmail.com>
2026-07-19 01:35:51 +0300

    100 advanced

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prime
+ * Return: void
+ */
+int main(void)
+{
+	unsigned long n = 612852475143, i;
+
+	for (i = 2; i * i <= n; i++)
+		while (n % i == 0)
+		{
+			printf("%lu ", i);
+			n = n / i;
+		}
+	if (n > 1)
+		printf("%lu", n);
+	printf("\n");
+}
