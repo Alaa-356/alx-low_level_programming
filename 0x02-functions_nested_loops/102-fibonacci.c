9a611886b94061b756b90a57b7a12c5f7d7fa46b
Alaa-356 <alaaaemm@gmail.com>
2026-07-18 13:19:51 +0300

    program 102 advance

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: program that prints the first 50 Fibonacci numbers,
+ * 		starting with 1 and 2, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long x = 1, y = 2, fib;
+	int i;
+
+	printf("%lu, %lu", x, y);
+	for ( i = 3; i <= 50; i++)
+	{
+		fib = x + y;
+		printf(", %lu", fib);
+		x = y;
+		y = fib;
+	}
+	printf("\n");
+	return (0);
+}
