72bddbbb32db0e4603e4900018ced15d9286d473
Alaa-356 <alaaaemm@gmail.com>
2026-07-19 22:12:18 +0300

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - pro name
+ * Description: to reset the value of n to 98
+ * @n: the variable to be changed to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
